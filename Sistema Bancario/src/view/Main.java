b5d4c37e6ababbfda17ae0cc6306a3321353450c
Nathalia Lyra Varela de Albuquerque <nathalialyra@ufpr.br>
2025-12-01 21:36:43 -0300

    Add comments to Main.java for clarity

--- Sistema Bancario/src/view/Main.java
+++ Sistema Bancario/src/view/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); // pega o estilo nativo do sistema operacional
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -14,9 +14,9 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                TelaManterClientes telaPrincipal = new TelaManterClientes();
-                telaPrincipal.setLocationRelativeTo(null);
-                telaPrincipal.setVisible(true);
+                TelaManterClientes telaPrincipal = new TelaManterClientes(); // Cria tela principal
+                telaPrincipal.setLocationRelativeTo(null); // Centraliza a janela na tela
+                telaPrincipal.setVisible(true); // Exibe a janela para o usuário.
             }
         });
     }
